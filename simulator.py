116ebe12006a4865cd451a81b954752dff4069ea
zachsez <zachsez@gmail.com>
2018-10-13 16:44:43 -0500

    Simulator can add effectively

--- simulator.py
+++ simulator.py
@@ -1,3 +1,6 @@
+import sys
+import collections
+
 # Class CPU will hold the information of the CPU
 class CPU:
     PC = 0 # Program Counter
@@ -28,7 +31,10 @@ def load_program(cpu, instr_file_name, memory_file_name):
         line = line.strip()
         if len(line) < 1 or line.startswith("#"):
             continue
-        cpu.memory.append(int(line,2))
+        number = int(line,2)
+        if line.startswith("1"):
+            number = (0xFFFF - int(line,2) + 1) * -1
+        cpu.memory.append(number)
 
     for i in range(128-len(cpu.memory)):
         cpu.memory.append(0)
@@ -38,7 +44,66 @@ def load_program(cpu, instr_file_name, memory_file_name):
 
     return cpu
 
+def run_program(cpu):
+    finished = False
+    while(not finished):
+        instr = cpu.instructions[cpu.PC]
+        if not check_parity_bit(instr):
+            print("ERROR: Parity Bit Error")
+            sys.exit()
+
+        if instr[1:4] == "000":
+            # Load instruction
+            Rx = registers[instr[4:6]]
+            Ry = registers[instr[6:8]]
+            cpu.R[Rx] = cpu.memory[cpu.R[Ry]]
+            cpu.PC = cpu.PC + 1
+            cpu.DIC = cpu.DIC + 1
+        elif instr[1:4] == "001":
+            # Store instruction
+            Rx = registers[instr[4:6]]
+            Ry = registers[instr[6:8]]
+            cpu.memory[cpu.R[Ry]] = cpu.R[Rx]
+            cpu.PC = cpu.PC + 1
+            cpu.DIC = cpu.DIC + 1
+        elif instr[1:4] == "010":
+            # Add instruction
+            Rx = registers[instr[4:6]]
+            Ry = registers[instr[6:8]]
+            cpu.R[Rx] = cpu.R[Rx] + cpu.R[Ry]
+            cpu.PC = cpu.PC + 1
+            cpu.DIC = cpu.DIC + 1
+        elif instr[1:4] == "101":
+            # Init instruction
+            Rx = registers[instr[4:6]]
+            value = {"11" : -2,
+                     "10" : -1,
+                     "00" : 0,
+                     "01" : 1}
+            cpu.R[Rx] = value[instr[6:8]]
+            cpu.PC = cpu.PC + 1
+            cpu.DIC = cpu.DIC + 1
+        elif instr[1:6] == "11010":
+            Rx = registers[instr[6:8]]
+            if cpu.R[Rx] == 0:
+                finished = True
+            cpu.PC = cpu.PC + cpu.R[Rx]
+            cpu.DIC = cpu.DIC + 1
+        else:
+            print("Error Unknown command")
+            sys.exit()
+    return cpu
+
+registers = {"00" : 0,
+             "01" : 1,
+             "10" : 2,
+             "11" : 3}
+
 if __name__ == "__main__":
     cpu1 = CPU()
     cpu1 = load_program(cpu1, "sample_program_machine.ctz", "sample_memory.txt")
+    print(cpu1.memory[0:5])
+    cpu1 = run_program(cpu1)
+    print(cpu1.R)
+    print(cpu1.DIC)
     print(cpu1.memory[0:5])
